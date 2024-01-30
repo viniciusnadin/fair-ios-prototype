5a700ade8485a52d8ee224d78eaa1daf69bb04e4
Vinicius Nadin <vininadin@gmail.com>
2024-01-30 15:34:42 -0300

    Initial Commit

---
+++ FairPrototype/FairPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FairPrototypeApp.swift
+//  FairPrototype
+//
+//  Created by Vinicius Nadin on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct FairPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
