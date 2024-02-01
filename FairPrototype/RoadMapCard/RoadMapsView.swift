2c8aea8b9d8009d486399d1a340b6c6da5a3e332
Vinicius Nadin <vininadin@gmail.com>
2024-02-01 10:45:05 -0300

    Implements notifications button

--- FairPrototype/RoadMapCard/RoadMapsView.swift
+++ FairPrototype/RoadMapCard/RoadMapsView.swift
@@ -10,23 +10,32 @@ import SwiftUI
 struct RoadMapsView: View {
     
     private var categories: [String] = ["Popular RoadMaps", "Enrolled RoadMaps"]
-    private var subCategories: [String] = ["Hotests", "Popular"]
+    private var subCategories: [String] = ["Hotests", "Popular", "New", "Recomended", "Top", "Favorites" ]
     
     var body: some View {
         VStack(alignment: .leading) {
-            HStack {
-                Image("ProfileImage")
-                    .resizable()
-                    .frame(width: 60, height: 60)
-                    .clipShape(Circle())
-                Spacer()
+            HStack(alignment: .top) {
+                VStack(alignment: .leading) {
+                    HStack(alignment: .center) {
+                        Image("ProfileImage")
+                            .resizable()
+                            .frame(width: 64, height: 64)
+                            .clipShape(Circle())
+                        Spacer()
+                        Image(systemName: "bell")
+                            .resizable()
+                            .foregroundStyle(.black)
+                            .frame(width: 24, height: 24)
+                            .padding()
+                    }
+                    .padding(.bottom, 5)
+                    
+                    Text("Vinicius")
+                        .font(.custom("Roboto-Light", size: 20))
+                        .foregroundStyle(.black.opacity(0.6))
+                        .padding(.bottom, 10)
+                }
             }
-            .padding(.bottom, 5)
-            
-            Text("Vinicius")
-                .font(.custom("Roboto-Light", size: 20))
-                .foregroundStyle(.black.opacity(0.6))
-                .padding(.bottom, 0)
             
             ScrollView(.horizontal) {
                 HStack {
