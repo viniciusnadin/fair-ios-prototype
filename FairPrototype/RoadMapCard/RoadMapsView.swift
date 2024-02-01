e58f4334587ac601ea69de9bd61104babf84b437
Vinicius Nadin <vininadin@gmail.com>
2024-02-01 00:17:46 -0300

    Implements subcategories view

--- FairPrototype/RoadMapCard/RoadMapsView.swift
+++ FairPrototype/RoadMapCard/RoadMapsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct RoadMapsView: View {
     
     private var categories: [String] = ["Popular RoadMaps", "Enrolled RoadMaps"]
+    private var subCategories: [String] = ["Hotests", "Popular"]
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -21,10 +22,23 @@ struct RoadMapsView: View {
                 Spacer()
             }
             .padding(.bottom, 5)
+            
             Text("Vinicius")
                 .font(.custom("Roboto-Light", size: 20))
                 .foregroundStyle(.black.opacity(0.6))
                 .padding(.bottom, 0)
+            
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(subCategories, id: \.self) { subCategorie in
+                        Text(subCategorie)
+                            .foregroundStyle(.white)
+                            .padding(10)
+                            .background(.black.opacity(0.8))
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                }
+            }.scrollIndicators(.hidden)
             
             ForEach(categories, id: \.self) { categorie in
                 CategoryRoadMapView(categoryTitle: categorie)
